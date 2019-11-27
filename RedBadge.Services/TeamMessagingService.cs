d648237fef94661e8505671be2bfa4cd29f682e6
jacobnewby <jacobnewby01@gmail.com>
2019-11-27 13:29:27 -0500

    Update

--- RedBadge.Services/TeamMessagingService.cs
+++ RedBadge.Services/TeamMessagingService.cs
@@ -1,86 +1,170 @@
 ﻿using RedBadge.Data;
+
 using RedBadge.Model;
+
 using System;
+
 using System.Collections.Generic;
+
 using System.IO;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
 
+
+
 namespace RedBadge.Services
+
 {
+
     public class TeamMessagingService
+
     {
+
         private readonly Guid _userID;
 
+
+
         public TeamMessagingService(Guid userID)
+
         {
+
             _userID = userID;
+
         }
+
+
 
         public bool CreateTeamMessaging(TeamMessagingCreate model)
-        {
-            byte[] bytes = null;
-            if (model.File != null){ 
 
-            Stream fs = model.File.InputStream;
-            BinaryReader br = new BinaryReader(fs);
-            bytes = br.ReadBytes((Int32)fs.Length);
+        {
+
+            byte[] bytes = null;
+
+            if (model.File != null)
+            {
+
+
+
+                Stream fs = model.File.InputStream;
+
+                BinaryReader br = new BinaryReader(fs);
+
+                bytes = br.ReadBytes((Int32)fs.Length);
+
+
 
             }
+
+
 
             var entity =
+
                 new TeamMessaging()
+
                 {
+
                     FileContent = bytes,
+
                     UserID = _userID,
+
                     Title = model.Title,
+
                     Content = model.Content,
-                    CreatedUtc = DateTime.Now,
+
+                    CreatedUtc = DateTimeOffset.Now,
+
                     TeamID = model.TeamID,
+
                 };
 
+
+
             using (var ctx = new ApplicationDbContext())
+
             {
-                ctx.TeamMessaging.Add(entity); 
+
+                ctx.TeamMessaging.Add(entity);
+
                 return ctx.SaveChanges() == 1;
+
             }
+
         }
 
-        public IEnumerable<TeamMessagingListItem> GetTeamMessages()
+
+
+        public IEnumerable<TeamMessagingListItem> GetTeamMessages(int TeamID)
+
         {
+
             using (var ctx = new ApplicationDbContext())
+
             {
-                var query =
+
+
+
+                var query1 =
+
                     ctx
-                    .TeamMessaging
-                    .Where(e => e.Roster.SingleOrDefault(i => i.UserID == _userID) != null)
-                    .Select(
-                        e =>
-                            new TeamMessagingListItem
-                            {
-                                FileContent = e.FileContent,
-                                MessageID = e.MessageID,
-                                Title = e.Title,
-                                Content = e.Content,
-                                CreatedUtc = e.CreatedUtc,
-                                Modifiedutc = e.Modifiedutc,
-                            }
-                        );
 
-                return query.ToArray();
+                    .TeamMessaging
+
+                    .Where(e => e.TeamID == TeamID && e.TeamVariable.Roster.FirstOrDefault(i => i.UserID == _userID) != null)
+
+                    .Select(
+
+                        e =>
+
+                            new TeamMessagingListItem
+
+                            {
+
+                                FileContent = e.FileContent,
+
+                                MessageID = e.MessageID,
+
+                                Title = e.Title,
+
+                                Content = e.Content,
+
+                                CreatedUtc = e.CreatedUtc,
+
+                                Modifiedutc = e.Modifiedutc,
+
+                                TeamID = e.TeamID,
+
+                                UserID = e.UserID
+
+                            }
+
+                        );
+                return query1.ToList();
             }
         }
+        public TeamMessagingDetail GetTeamMessageById(int id, int TeamID)
 
-        public TeamMessagingDetail GetTeamMessageById(int MessageID)
         {
             using (var ctx = new ApplicationDbContext())
             {
+
+                var entity1 =
+
+                    ctx
+                        .Team
+                        .Include("Roster")
+                        .Single(e => e.TeamID == TeamID);
+
                 var entity =
+
                     ctx
                         .TeamMessaging
-                        .Single(e => e.MessageID == MessageID);
-                if (entity.Roster.SingleOrDefault(e => e.UserID == _userID) != null)
+                        .Single(e => e.MessageID == id);
+                if (entity1.Roster.SingleOrDefault(e => e.UserID == _userID) != null)
+
                 {
                     return
                     new TeamMessagingDetail
@@ -95,25 +179,24 @@ namespace RedBadge.Services
                 {
                     return null;
                 }
-
             }
         }
-
         public bool UpdateTeamMessage(TeamMessagingEdit model)
+
         {
             using (var ctx = new ApplicationDbContext())
             {
                 byte[] bytes = null;
                 if (model.File != null)
                 {
-
                     Stream fs = model.File.InputStream;
                     BinaryReader br = new BinaryReader(fs);
                     bytes = br.ReadBytes((Int32)fs.Length);
-
                 }
 
+
                 var entity =
+
                     ctx
                         .TeamMessaging
                         .Single(e => e.MessageID == model.MessageID && e.UserID == _userID);
@@ -121,17 +204,26 @@ namespace RedBadge.Services
                 entity.FileContent = bytes;
                 entity.Title = model.Title;
                 entity.Content = model.Content;
-                entity.Modifiedutc = DateTime.Now;
+                entity.Modifiedutc = DateTimeOffset.Now;
 
                 return ctx.SaveChanges() == 1;
+
             }
+
         }
 
+
+
         public bool DeleteTeamMessage(int MessageID)
+
         {
+
             using (var ctx = new ApplicationDbContext())
+
             {
+
                 var entity =
+
                     ctx
                         .TeamMessaging
                         .Single(e => e.MessageID == MessageID && e.UserID == _userID);
@@ -139,7 +231,11 @@ namespace RedBadge.Services
                 ctx.TeamMessaging.Remove(entity);
 
                 return ctx.SaveChanges() == 1;
+
             }
+
         }
+
     }
+
 }
